eed664d4f07adab42ed08b46cee8e6ae743a377f
tuongtran1106 <tuongtran1106@gmail.com>
2024-07-17 11:56:52 +0700

    Using own token provider and inject token multiple another ways

--- src/app/tasks/tasks-list/task-item/task-item.component.ts
+++ src/app/tasks/tasks-list/task-item/task-item.component.ts
@@ -1,8 +1,9 @@
 import { Component, computed, inject, input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { Task, TaskStatus } from '../../task.model';
+import { Task, TASK_STATUS_OPTIONS, TaskStatus } from '../../task.model';
 import { TasksService } from '../../tasks.service';
+import { TasksServiceToken } from '../../../../main';
 
 @Component({
   selector: 'app-task-item',
@@ -12,7 +13,8 @@ import { TasksService } from '../../tasks.service';
   styleUrl: './task-item.component.css',
 })
 export class TaskItemComponent {
-  private tasksService = inject(TasksService);
+  private tasksService = inject(TasksServiceToken);
+  taskStatusOptions = inject(TASK_STATUS_OPTIONS);
   task = input.required<Task>();
   taskStatus = computed(() => {
     switch (this.task().status) {
@@ -43,7 +45,7 @@ export class TaskItemComponent {
       default:
         break;
       }
-      
+
       this.tasksService.updateTaskStatus(taskId, newStatus);
   }
 }
--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -1,11 +1,13 @@
-import { Injectable, signal } from "@angular/core";
+import { inject, Injectable, signal } from "@angular/core";
 import { Task, TaskStatus } from "./task.model";
+import { LoggingService } from "../logging.service";
 
-@Injectable({
-  providedIn: 'root',
-})
+// @Injectable({
+//   providedIn: 'root',
+// })
 export class TasksService {
   private tasks = signal<Task[]>([]);
+  private loggingService = inject(LoggingService);
 
   allTasks = this.tasks.asReadonly();
 
@@ -16,9 +18,11 @@ export class TasksService {
         status: 'OPEN'
     };
     this.tasks.update((oldTasks) => [...oldTasks, newTask]);
+    this.loggingService.log('ADDED TASK WITH TITLE ' + TaskData.title);
   }
 
   updateTaskStatus(taskId: string, newStatus: TaskStatus) {
     this.tasks.update((oldTasks) => oldTasks.map((task) => task.id === taskId ? {...task, status: newStatus} : task));
+    this.loggingService.log('CHANGE TASK STATUS TO ' + newStatus);
   }
 }
